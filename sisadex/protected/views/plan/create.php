82349dd8649d432eaf6d017180e69e3c0953304d
Dario Trossero <dariotrossero@gmail.com>
2015-07-08 10:01:13 -0300

    Verify changes before leave page for plans

--- sisadex/protected/views/plan/create.php
+++ sisadex/protected/views/plan/create.php
@@ -6,9 +6,15 @@ $this->widget('bootstrap.widgets.TbMenu', array(
     'type' => 'pills',
     'items' => array(
         array('label' => 'Nuevo', 'icon' => 'icon-plus', 'url' => Yii::app()->controller->createUrl('create'), 'active' => true, 'linkOptions' => array()),
-        array('label' => 'Listado', 'icon' => 'icon-th-list', 'url' => Yii::app()->controller->createUrl('index'), 'linkOptions' => array()),
+        array('label' => 'Listado', 'icon' => 'icon-th-list', 'url' =>Yii::app()->controller->createUrl('index'), 'linkOptions' => array()), 
 
     ),
 ));
 ?>
 <?php echo $this->renderPartial('_form', array('model' => $model)); ?>
+
+<script>
+
+
+    
+    </script>
